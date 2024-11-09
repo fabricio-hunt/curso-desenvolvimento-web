0274903a81a32abff53c1677f96b1f661f2e5c16
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2024-11-08 23:19:08 -0300

    if e else em js

---
+++ assets/ifEelse.js
@@ -0,0 +1,21 @@
+// Estrutura de controle if...else em JavaScript
+
+// Exemplo 1: if...else simples
+let hora = 10;
+
+if (hora < 12) {
+    console.log("Bom dia!");
+} else {
+    console.log("Boa tarde!");
+}
+
+// Exemplo 2: if...else if...else com múltiplas condições
+let nota = 85;
+
+if (nota >= 90) {
+    console.log("Excelente!");
+} else if (nota >= 70) {
+    console.log("Bom!");
+} else {
+    console.log("Precisa melhorar.");
+}
